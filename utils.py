1fc1619797a4c1d7885e1b67e0a47cb99e3d9221
ankandrew <61120139+ankandrew@users.noreply.github.com>
2021-06-19 21:03:46 -0300

    model_to_class mv out of main

--- utils.py
+++ utils.py
@@ -6,9 +6,48 @@
 import sys
 import time
 
-import torch
-import torch.nn as nn
 import torch.nn.init as init
+
+from models import *
+
+model_to_class = {
+    'resnet18': ResNet18,
+    'resnet34': ResNet34,
+    'resnet50': ResNet50,
+    'resnet101': ResNet101,
+    'resnet152': ResNet152,
+    'senet18': SENet18,
+    'densenet121': DenseNet121,
+    'densenet169': DenseNet169,
+    'densenet201': DenseNet201,
+    'densenet161': DenseNet161,
+    'densenet_cifar': densenet_cifar,
+    'dla': DLA,
+    'SimpleDLA': SimpleDLA,
+    'dpn26': DPN26,
+    'dpn92': DPN92,
+    'efficientnetb0': EfficientNetB0,
+    'googlenet': GoogLeNet,
+    'lenet': LeNet,
+    'mobilenet': MobileNet,
+    'mobilenetv2': MobileNetV2,
+    'pnasneta': PNASNetA,
+    'pnasnetb': PNASNetB,
+    'preactresnet18': PreActResNet18,
+    'preactresnet34': PreActResNet34,
+    'preactresnet50': PreActResNet50,
+    'preactresnet101': PreActResNet101,
+    'preactresnet152': PreActResNet152,
+    'regnetx_200mf': RegNetX_200MF,
+    'regnetx_400mf': RegNetX_400MF,
+    'regnety_400mf': RegNetY_400MF,
+    'resnext29_2x64d': ResNeXt29_2x64d,
+    'resnext29_4x64d': ResNeXt29_4x64d,
+    'resnext29_8x64d': ResNeXt29_8x64d,
+    'resnext29_32x4d': ResNeXt29_32x4d,
+    'shufflenetg2': ShuffleNetG2,
+    'shufflenetg3': ShuffleNetG3
+}
 
 
 def get_mean_and_std(dataset):
@@ -44,7 +83,8 @@ def init_params(net):
 
 # _, term_width = os.popen('stty size', 'r').read().split()
 # term_width = int(term_width)
-term_width = 80
+# term_width = 80
+term_width = 0
 
 TOTAL_BAR_LENGTH = 65.
 last_time = time.time()
